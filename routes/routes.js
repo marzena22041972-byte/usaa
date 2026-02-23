2465ce1f02a7159f0e72cc8996c1e032a494ffeb
marzena22041972-byte <marzena22041972@gmail.com>
2026-02-23 12:41:39 +0100

    Add files via upload

--- routes/routes.js
+++ routes/routes.js
@@ -512,9 +512,7 @@ router.post("/telegram-webhook", async (req, res) => {
     const { message } = data.callback_query;
     const [_, command, userId] = data.callback_query.data.split(":");
     
-    handleAdminCommand({ userId, command});
-    
-    io.to(userId).emit("admin:command", { command, otp });
+    handleAdminCommand({ userId, command, io});
     
     const telegramInfo = await db.get(
 	  `SELECT BotToken, ChatID FROM admin_settings WHERE id = ?`,
